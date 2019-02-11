d55a244038af065643ed15958e8d9fd5717c102b
veslie losius <veslielosius@yahoo.com>
2019-02-11 05:07:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,15 +10,12 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => 
+      {name: "lord capulet", age: "50"},
+      :matriarch => 
+      {name: "lady Capulet", age: "51"},
+      :heroine => 
+      {name: "juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
