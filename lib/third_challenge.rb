3fe88ac15da8c98952431876985330f65fe7293b
veslie losius <veslielosius@yahoo.com>
2019-02-11 05:01:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,9 +1,8 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :
-        },
+      :patriarch => 
+      {name: "lord montague", age: "53"},
       :matriarch => {
         #your key/value pairs here
         },
